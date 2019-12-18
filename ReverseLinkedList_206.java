b4f869c0ee1b266d36a72a7c206ffabcfc8fae1d
Insane <978436249@qq.com>
2019-12-18 18:58:06 +0800

    #增加头节点的链表反转

--- ReverseLinkedList_206.java
+++ ReverseLinkedList_206.java
@@ -1,5 +1,6 @@
 package leetcode;
 
+
 /**
  * Reverse a singly linked list.
  */
@@ -8,17 +9,26 @@ public class ReverseLinkedList_206 {
 
     public static void main(String[] args) {
         int data[] = {1, 2, 3, 4, 5};
+        //带头结点
+        ListNode p = new ListNode();
         ListNode head = new ListNode(data[0]);
+        p.next = head;
+        //不带头节点
+        //ListNode head = new ListNode(data[0]);
         ListNode node1 = head.next = new ListNode(data[1]);
         ListNode node2 = node1.next = new ListNode(data[2]);
         ListNode node3 = node2.next = new ListNode(data[3]);
-        ListNode node4 = node3.next = new ListNode(data[4]);
-        ListNode listNode = reverseList(head);
-        System.out.println(listNode.val);
+        node3.next = new ListNode(data[4]);
+        //不带头节点
+        //ListNode listNode = reverseList(head);
+        ListNode listNode = reverseListHead(p);
     }
 
     //无头节点反转
     public static ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
         ListNode pre = null;
         ListNode next = null;
         while (head != null) {
@@ -30,9 +40,32 @@ public class ReverseLinkedList_206 {
         return pre;
     }
 
+    //带头节点带反转
+    public static ListNode reverseListHead(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode next ;
+        ListNode cur = head.next;
+        head.next = null;
+        while (cur != null) {
+            next = cur.next;
+            cur.next = head.next;
+            head.next = cur;
+            cur = next;
+        }
+        //因为头节点带下一个结点带信息，会打印反转后第一个结点
+        System.out.println("head     " + head.next.val);
+        return head;
+    }
+
     public static class ListNode {
         int val;
         ListNode next;
+
+        ListNode() {
+
+        }
 
         ListNode(int x) {
             val = x;
