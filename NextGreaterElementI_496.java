040b2c3391d9bce723a72577ad101c10fe33a015
wubobo952 <13536458246@163.com>
2020-01-18 17:07:08 +0800

    下一个更大元素 I

---
+++ NextGreaterElementI_496.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+/**
+ * 496. 下一个更大元素 I
+ */
+public class NextGreaterElementI_496 {
+    public static void main(String[] args) {
+        int nums1[] = {1, 3, 5, 2, 4};
+        int nums2[] = {6, 5, 4, 3, 2, 1, 7};
+        nextGreaterElement(nums1, nums2);
+    }
+
+    /**
+     *暴力法
+     */
+    public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] result = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums1[i] == nums2[j]) {
+                    for (int k = j; k < nums2.length; k++) {
+                        if (nums1[i] < nums2[k]) {
+                            result[i] = nums2[k];
+                        } else {
+                            result[i] = -1;
+                        }
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
