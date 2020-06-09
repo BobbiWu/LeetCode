4ef5feeed885bf871ba24c6d47e60d584303a636
wubobo952 <13536458246@163.com>
2020-06-09 23:46:28 +0800

    爬楼梯

---
+++ ClimbingStairs_70.java
@@ -0,0 +1,46 @@
+package leetcode;
+
+/**
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ * <p>
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ * <p>
+ * 注意：给定 n 是一个正整数。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入： 2
+ * 输出： 2
+ * 解释： 有两种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶
+ * 2.  2 阶
+ * 示例 2：
+ * <p>
+ * 输入： 3
+ * 输出： 3
+ * 解释： 有三种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶 + 1 阶
+ * 2.  1 阶 + 2 阶
+ * 3.  2 阶 + 1 阶
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/climbing-stairs
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class ClimbingStairs_70 {
+    public static void main(String[] args) {
+        System.out.println(climbStairs(6));
+    }
+
+    //斐波那契
+    public static int climbStairs(int n) {
+        if (n <= 2) return n;
+        int[] ints = new int[n + 1];
+        ints[1] = 1;
+        ints[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            ints[i] = ints[i - 1] + ints[i - 2];
+        }
+        return ints[n];
+    }
+}
