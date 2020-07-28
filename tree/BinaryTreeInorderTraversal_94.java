23d0c65a770f1651ac1c8ab8ca4365782e3dd239
wubobo952 <13536458246@163.com>
2020-07-28 20:29:08 +0800

    94. 给定一个二叉树返回它的中序遍历(非递归)

--- tree/BinaryTreeInorderTraversal_94.java
+++ tree/BinaryTreeInorderTraversal_94.java
@@ -1,7 +1,10 @@
 package leetcode.tree;
 
+import com.sun.source.tree.Tree;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * 给定一个二叉树，返回它的中序 遍历。
@@ -33,31 +36,55 @@ public class BinaryTreeInorderTraversal_94 {
     public static void main(String[] args) {
         TreeNode treeNode = new TreeNode(1);
         TreeNode treeNode1 = treeNode.right = new TreeNode(2);
-        treeNode1.left=new TreeNode(3);
-        inorderTraversal(treeNode);
+        treeNode1.left = new TreeNode(3);
+//        inorderTraversal(treeNode);
+        System.out.println(inorderTraversal2(treeNode));
     }
 
     public static List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList<>();
-        findNode(root,list);
+        findNode(root, list);
         return list;
     }
 
     /**
-     *递归
+     * 递归
      */
-    public static void findNode(TreeNode root,List<Integer> res) {
-        if (root!=null){
-          if (root.left!=null){
-              findNode(root.left,res);
-          }
-          res.add(root.val);
-          System.out.println(root.val);
-          if (root.right!=null){
-              findNode(root.right,res);
-          }
+    public static void findNode(TreeNode root, List<Integer> res) {
+        if (root != null) {
+            if (root.left != null) {
+                findNode(root.left, res);
+            }
+            res.add(root.val);
+            System.out.println(root.val);
+            if (root.right != null) {
+                findNode(root.right, res);
+            }
         }
 
+    }
+
+    /**
+     * 非递归
+     */
+    public static List<Integer> inorderTraversal2(TreeNode root) {
+        ArrayList<Integer> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode currentNode = root;
+        while (currentNode != null || !stack.isEmpty()) {
+            //查找左子树全部进栈
+            while (currentNode != null) {
+                stack.push(currentNode);
+                currentNode = currentNode.left;
+            }
+            //出栈存并且进去数组
+            currentNode = stack.pop();
+            list.add(currentNode.val);
+            //查找右子树
+            currentNode = currentNode.right;
+
+        }
+        return list;
     }
 
     public static class TreeNode {
